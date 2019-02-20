02457b084392232819d3d36dd3b1f8a768b67f2f
Krzysztof Otrebski <krzysztof.otrebski@gmail.com>
2019-02-21 00:12:07 +0100

    Update credit implementation and tests

--- src/main/scala/vending/VendingMachineSm.scala
+++ src/main/scala/vending/VendingMachineSm.scala
@@ -1,8 +1,28 @@
 package vending
 
+import cats.data.State
+import cats.syntax.option._
 import vending.Domain._
 
 object VendingMachineSm {
+
+  //    State monad is function
+  // ╭----------------------------╮
+  // |  State => (State, Effect)  |
+  // ╰----------------------------╯
+  //
+  // In our case
+  // Vending machine state => (New vending machine state, effects)
+
+
+  def updateCredit(action: Action): State[VendingMachineState, Option[UserOutput]] =
+    State[VendingMachineState, Option[UserOutput]] { s =>
+      action match {
+        case Credit(value) => (s.copy(credit = s.credit + value), CreditInfo(s.credit + value).some)
+        case Withdrawn => (s.copy(credit = 0), CollectYourMoney.some)
+        case _ => (s, none[UserOutput])
+      }
+    }
 
   case class VendingMachineState(credit: Int,
                                  income: Int,
--- src/test/scala/vending/VendingMachineSmTest.scala
+++ src/test/scala/vending/VendingMachineSmTest.scala
@@ -1,8 +1,24 @@
 package vending
 
+import java.time.LocalDate
+
+import cats.syntax.option._
 import org.scalatest.{Matchers, WordSpec}
+import vending.Domain._
+import vending.VendingMachineSm.VendingMachineState
 
 class VendingMachineSmTest extends WordSpec with Matchers {
+
+  private val beer = Product(3, "1", Symbols.beer, LocalDate.of(2020, 12, 10))
+  private val pizza = Product(100, "2", Symbols.pizza, LocalDate.of(2018, 12, 10))
+
+  var vendingMachineState = VendingMachineState(
+    credit = 0, income = 0,
+    quantity = Map(
+      beer -> 5,
+      pizza -> 1
+    )
+  )
 
   "Vending machine" should {
     "successfully buy and give change" in ???
@@ -24,8 +40,28 @@ class VendingMachineSmTest extends WordSpec with Matchers {
   }
 
   "update credit monad" should {
-    "update credit when insert" in ???
-    "clear credit when withdrawn is selected" in ???
+
+    "update credit when insert" in {
+
+      val (state, (effects1, effects2)) = (for {
+        e1 <- VendingMachineSm.updateCredit(Credit(2))
+        e2 <- VendingMachineSm.updateCredit(Credit(5))
+      } yield (e1, e2)).run(vendingMachineState).value
+
+      state.credit shouldBe 7
+      effects1 shouldBe CreditInfo(2).some
+      effects2 shouldBe CreditInfo(7).some
+    }
+
+    "clear credit when withdrawn is selected" in {
+      val state0 = vendingMachineState.copy(credit = 20)
+
+      val (state1, effects) = VendingMachineSm.updateCredit(Withdrawn).run(state0).value
+
+      state1.credit shouldBe 0
+      effects shouldBe CollectYourMoney.some
+    }
+
   }
 
   "select product monad" should {
